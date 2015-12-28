c23b41079fe514a01c0ac55358ccbc38a23bdea8
Rohan Kasiviswanathan <rohan.kasivis@gmail.com>
2015-12-27 17:39:01 -0800

    Basic Button Handling Complete

---
+++ app/src/main/java/com/example/dennis/leagueoflegions/Game.java
@@ -0,0 +1,5 @@
+package com.example.dennis.leagueoflegions;
+
+public class Game {
+    public final static String GAME_MODE = "game_mode";
+}
--- app/src/main/java/com/example/dennis/leagueoflegions/GameActivity.java
+++ app/src/main/java/com/example/dennis/leagueoflegions/GameActivity.java
@@ -1,20 +1,18 @@
 package com.example.dennis.leagueoflegions;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.EditText;
 
-public class SplashScreenActivity extends AppCompatActivity {
+public class GameActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
+        setContentView(R.layout.activity_game);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -26,23 +24,8 @@ public class SplashScreenActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-    }
 
-    public void startGame(View view) {
-        // Do something in response to button
-        Intent intent = new Intent(this, GameActivity.class);
-
-        switch (view.getId()) {
-            case R.id.button_campaign:
-                intent.putExtra(Game.GAME_MODE, "campaign");
-                break;
-            case R.id.button_multiplayer:
-                intent.putExtra(Game.GAME_MODE, "multiplayer");
-                break;
-            case R.id.button_tutorial:
-                intent.putExtra(Game.GAME_MODE, "tutorial");
-                break;
-        }
-        startActivity(intent);
+        Bundle extras = getIntent().getExtras();
+        String game_mode = extras.getString(Game.GAME_MODE);
     }
 }
