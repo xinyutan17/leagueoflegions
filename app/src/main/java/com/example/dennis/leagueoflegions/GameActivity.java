c76872be050656523b5c7865e71f4408b0a2323b
Xin Yu Tan <xinyutan@berkeley.edu>
2015-12-30 17:28:01 -0800

    removed MainActivity and changed Manifest

--- app/src/main/java/com/example/dennis/leagueoflegions/GameActivity.java
+++ app/src/main/java/com/example/dennis/leagueoflegions/GameActivity.java
@@ -1,29 +1,14 @@
 package com.example.dennis.leagueoflegions;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 
-public class GameActivity extends AppCompatActivity {
+public class GameActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         Bundle extras = getIntent().getExtras();
         String game_mode = extras.getString(Game.GAME_MODE);
--- app/src/main/java/com/example/dennis/leagueoflegions/SplashScreenActivity.java
+++ app/src/main/java/com/example/dennis/leagueoflegions/SplashScreenActivity.java
@@ -1,31 +1,16 @@
 package com.example.dennis.leagueoflegions;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.EditText;
 
-public class SplashScreenActivity extends AppCompatActivity {
+public class SplashScreenActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     public void startGame(View view) {
@@ -44,5 +29,6 @@ public class SplashScreenActivity extends AppCompatActivity {
                 break;
         }
         startActivity(intent);
+//        finish();
     }
 }
