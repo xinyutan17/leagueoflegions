83079de5320a156fa033da81b544d78e58b9cdda
Xin Yu Tan <xinyutan@berkeley.edu>
2016-01-07 18:10:12 -0800

    android example code for opengl with triangle and square

--- app/src/main/java/com/example/dennis/leagueoflegions/GLActivity.java
+++ app/src/main/java/com/example/dennis/leagueoflegions/GLActivity.java
@@ -1,45 +1,53 @@
+/*
+ * Copyright (C) 2011 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.dennis.leagueoflegions;
 
 import android.app.Activity;
-import android.opengl.GLSurfaceView;
 import android.os.Bundle;
-import android.view.Window;
-import android.view.WindowManager;
 
 public class GLActivity extends Activity {
 
-    // Our OpenGL Surfaceview
-    private GLSurfaceView glSurfaceView;
+    private GLSurface mGLView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-
-        // Turn off the window's title bar
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-
-        // Super
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Fullscreen mode
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
-        // We create our Surfaceview for our OpenGL here.
-        glSurfaceView = new GLSurface(this);
-
-        // Set our view.
-        setContentView(glSurfaceView);
+        // Create a GLSurfaceView instance and set it
+        // as the ContentView for this Activity
+        mGLView = new GLSurface(this);
+        setContentView(mGLView);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        glSurfaceView.onPause();
+        // The following call pauses the rendering thread.
+        // If your OpenGL application is memory intensive,
+        // you should consider de-allocating objects that
+        // consume significant memory here.
+        mGLView.onPause();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        glSurfaceView.onResume();
+        // The following call resumes a paused rendering thread.
+        // If you de-allocated graphic objects for onPause()
+        // this is a good place to re-allocate them.
+        mGLView.onResume();
     }
-
 }
